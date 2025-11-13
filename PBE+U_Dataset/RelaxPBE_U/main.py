91dd9262501d9e306e4f35914dcd3cbcc7fa9b90
jvc perseu <victorjvc2020@gmail.com>
2025-11-13 12:08:13 -0300

    Update main with U >= 9.5 calculations

--- PBE+U_Dataset/RelaxPBE_U/main.py
+++ PBE+U_Dataset/RelaxPBE_U/main.py
@@ -1,45 +1,64 @@
 from pathlib import Path
 import pandas as pd
-import pprint
+from pprint import pprint
 from ase.io import read
 from ase.units import Bohr
 
-files: list[Path] = list(Path('hubbard_scans').glob('zno_vcrelax_hubbard_*.out'))
+files: list[Path] = list(
+    Path('hubbard_scans').glob('zno_vcrelax_hubbard_*.out')
+    )
 
-data_gaps = {}
+print(f'Total number of files = {len(files)}')
+idxs_to_drop: list[int] = []
+for idx, file in enumerate(files):
+    with open(file, 'r') as f:
+        data = f.readlines()
+        for line in data:
+            if 'Error in routine bfgs (1)' in line:
+                idxs_to_drop.append(idx)
 
-for file in files:
-    text: list[str] = file.read_text().split('\n')
-    for idx, line in enumerate(text):
-        
-        # Band Gap
-        if line.startswith('     highest occupied'):
-            highest, lowest = line.split(':')[-1:][0].strip().split()
+print(f'Total BFGS Errors {len(idxs_to_drop)}')
+converged_files = [file for i, file in enumerate(files) if i not in idxs_to_drop]
+print(f'Converged Files: {len(converged_files)}\n')    
+
+# ======================= Extract Physical Data ============================== #
+
+data_gaps: dict[Path, float] = {}
+extract_gap_from_line = lambda x : x.split(':')[-1:][0].strip().split()
+
+for file in converged_files:
+    text = file.read_text().split('\n') 
+    for line in text:
+        if ('     highest occupied') in line:
+            highest, lowest = extract_gap_from_line(line)
             band_gap = float(lowest) - float(highest)
-            # ZnO Band Gap ~ 3.37
-            band_gap = round(band_gap, 2) 
-            # print(f'{highest} {lowest} -> band gap: {float(lowest) - float(highest)}')
-            data_gaps[file] = band_gap
-        
-
-data_gaps = dict(sorted(data_gaps.items(), key=lambda item: item[1], reverse=True))
-
-good_band_gaps = {k:v for k,v in data_gaps.items() if v > 3.2 and v < 3.4}
+            if band_gap > 3.2 and band_gap < 3.4: 
+                #print(file.name, round(band_gap, 3))
+                data_gaps[file] = round(band_gap,3)
 
 
+data_gaps = dict(
+    sorted(data_gaps.items(), key= lambda item: item[1]) # sort by Egap
+)
 
-print('\n{:>35} | {:^10} | {:^10} | {:^10} | {:^10} |'.format('File', 'a (Å)', 'c (Å)', 'c/a', 'Egap (eV)'))
-print('-' * 90)
-
-for file, Egap in good_band_gaps.items():
-    atoms = read(file)
+def get_lattice_params(file: Path, gap_dict: dict):
+    atoms = read(file, format='espresso-out')
     a, b, c = atoms.cell.lengths()
-    print('{:>35} | {:10.4f} | {:10.4f} | {:10.4f} | {:10.2f} |'.format(
-        file.name, a, c, c/a, Egap))
+    ratio_ca = c/a
+    egap = gap_dict[file]
 
-    if Egap == 3.37:
-        print(f'\nBest Result for energy band gap: {file.name}:')
-        print(f'\tAtomic Positons:')
-        print(f'{atoms.get_scaled_positions()}\n')
-    else:
-        continue
+    return {file.name:  [a, c, ratio_ca, egap] }
+
+phys_info = [get_lattice_params(f, data_gaps) for f in data_gaps.keys()]
+
+columns = ['File', 'a (Å)', 'c (Å)', 'c/a Ratio', 'Band Gap (eV)']
+
+phys_info_df = pd.DataFrame(
+    [ [file, *values] for info in phys_info for file, values in info.items()],
+    columns=columns
+)
+
+
+# phys_info_df.to_csv('vcrelax_physical_info.csv', index=False)
+pprint(phys_info_df)
+
