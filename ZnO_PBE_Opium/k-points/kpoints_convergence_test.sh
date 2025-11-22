ce5cfafb43710ae51752a80f77cae6a796854952
jvc perseu <victorjvc2020@gmail.com>
2025-11-22 18:13:48 -0300

    Opium PBE-GGA Kpoint Convergence

--- ZnO_PBE_Opium/k-points/kpoints_convergence_test.sh
+++ ZnO_PBE_Opium/k-points/kpoints_convergence_test.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# Convergence test of k-points grid.Set a variable k-point from 4 to 12.
+
+for k in $(seq 4 1 12); do
+# Make input file for the SCF calculation.
+cat > scf_kpoint.$k.in << EOF
 !Optimal Lattice Parameters (Bohr):
 !	Lattice Parameter a = 6.1003
 !	Lattice Parameter c = 9.8520
@@ -6,7 +12,7 @@
   calculation = 'scf',
   pseudo_dir = '../pseudos_PBE_Opium',
   outdir='.'
-  prefix = 'ZnO_opium_12',
+  prefix = 'ZnO_opium_${k}',
   verbosity = 'high',
   tstress=.true.,
   tprnfor=.true.,
@@ -38,10 +44,19 @@ Zn   65.380    Zn.upf
  O   15.999    O.upf
 
 K_POINTS automatic
- 12 12 12  0  0  0
+ $k $k ${k-2}  0  0  0
 
 ATOMIC_POSITIONS (crystal)
 Zn               0.6666666667        0.3333333333        0.5006738064
 Zn               0.3333333333        0.6666666667        0.0006738064
 O                0.6666666667        0.3333333333        0.8796361936
 O                0.3333333333        0.6666666667        0.3796361936
+EOF
+
+# Run pw.x for SCF calculation.
+mpirun -np 16 "$HOME/pw_intel.x" < scf_kpoint.$k.in > scf_kpoint.$k.out
+
+# Write the number of k-points (= k*k*1) and the total energy in calc-kpoint.dat
+awk -v k=$k '/!/ {printf "%dx%dx%d %s\n", k, k, k-2, $5}' scf_kpoint.$k.out >> kpoints_convergence_energies.dat
+# End of for loop.
+done
