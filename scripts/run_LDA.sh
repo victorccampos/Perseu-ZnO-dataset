8f65630377b79434513957e6fe295279a9f147a7
jvc perseu <victorjvc2020@gmail.com>
2026-03-04 18:00:52 -0300

    Adicionado envio de e-mail.

--- scripts/run_LDA.sh
+++ scripts/run_LDA.sh
@@ -3,10 +3,10 @@
 # 1. Lista os arquivos .in
 # 2. Usa o '-' como delimitador para ordenar pela 4ª coluna (-k4,4) numericamente (-n)
 # 3. Itera sobre essa lista ordenada
-
+JOBNAME="LDA_006"
 echo "Início de Criação do Dataset [$(date +%F)] $(date +%T)"
 
-input_dir="LDA_000_INPUTS"
+input_dir="LDA_004_INPUTS"
 cd $input_dir
 
 files=$(ls ZnO-*.in | sort -t '-' -k4,4n)
@@ -17,9 +17,12 @@ for file in $files; do
     echo
     echo "------------- RUN [$(date +%F)] $(date +%T) -------------"
     echo "[IN/OUT]: $file --> $output "
-    #mpirun -np 32 pw.x -nk 2 < "$file" > "../LDA_000_OUTPUTS/$output"
+    mpirun -np 32 pw.x -nk 2 < "$file" > "../LDA_004_OUTPUTS/$output"
     
     echo "FINALIZADO: [$(date +%F)] $(date +%T)"
     echo "---------------------------------------------------------"
     echo
 done
+
+# Manda um e-mail avisando que o processo terminou
+python send_email.py "$JOBNAME finished!"
