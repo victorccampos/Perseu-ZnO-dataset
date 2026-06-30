a296b0f869959587eb718f3dbe9468f072329d64
joaovc_radija <joaovictorcjvc9@gmail.com>
2026-06-30 14:34:57 -0300

    DELETE benchmark

--- nnp_physics_validation/bench_aenet_lammps/bench.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for NP in 40 48 56 60 64; do
-    echo "Running 2000 steps with np = $NP:"
-    mpirun -np "$NP" lmp_mpi -log "${NP}procs.log" -in in.benchmark  > "${NP}.procs.out"
-done
-
-echo "Benchmark concluído"
